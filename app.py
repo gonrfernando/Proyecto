7856dd1ef7a09563b1f4eaa72b34aeb8407181d0
Fernando Gonzalez <gonr.fernando@gmail.com>
2026-02-27 02:04:04 -0600

    Version parcial 1 comentada

--- app.py
+++ app.py
@@ -1,148 +1,172 @@
 import os
+import re
 from flask import Flask, render_template, request, jsonify, redirect, url_for, session
 
 app = Flask(__name__)   
+#Necesitamos una llave secreta para usar sesiones
 app.secret_key = "Fanta de Limon"
 
-@app.route("/")
+@app.route("/") #Mostramos home.html cuando el link es "/" o vacio
 def home():
     return(render_template('home.html'))
 
-@app.route("/Tablas")
+
+@app.route("/Tablas") #Mostramos tablas.html cuando el link es "/Tablas"
 def tablas():
-    return(render_template('tablas.html'))
+    return(render_template('tablas.html')) 
 
-@app.route('/Generar-Tablas', methods=['POST'])
-def generar_tablas():
-    return jsonify({'message': 'Generar Tablas endpoint'})
 
-@app.route('/resultado')
+@app.route('/resultado') #Mostramos resultado.html cuando el link es "/resultado". Esta ruta se llama desde el main.js despues de hacer la tabla de verdad, para mostrarla
 def resultado():
+    # Va por las variables a la sesion (el navegador)
     encabezados = session.get('encabezados', [])
     tabla = session.get('tabla', [])
-    return(render_template('resultado.html', encabezados=encabezados, tabla=tabla))
+    metodo = session.get('metodo')
+    renglones_criticos = session.get('renglones_criticos', [])
+    renglones_falla = session.get('renglones_falla', [])
+    argumento_valido = session.get('argumento_valido', True)
+    
+    #Manda a mostrar el resultado.html y le pasa los encabezados y la tabla como variables al html
+    return(render_template('resultado.html', encabezados=encabezados, tabla=tabla, metodo=metodo, renglones_criticos=renglones_criticos, renglones_falla=renglones_falla, argumento_valido=argumento_valido)) 
 
 
-@app.route('/renglon-critico', methods=['POST'])
+@app.route('/renglon-critico', methods=['POST']) #se manda a llamar desde main.js cuando se usa el boton de renglon critico
 def renglon_critico():
+    #Recibimos los datos json que mando el html
     data = request.get_json()
-    hypotheses = data.get('hypotheses', [])
+    arreglo_hipotesis = data.get('hypotheses', [])
     conclusion = data.get('conclusion', '')
-    tabla = []
     
-    variables = set()
+    #Generamos la tabla. Le mandamos las hipotesis, la conclusion y que es con renglon critico
+    encabezados, tabla, argumento_valido, renglones_criticos, renglones_falla = generar_tabla_verdad(arreglo_hipotesis, conclusion, "renglon_critico")
     
-    for i in range(len(hypotheses)):
-        variables.update(extraer_variables(hypotheses[i]))
-        
-    variables = list(variables)
-    variables.sort()
-        
-    encabezados = []
-    for var in variables:
-        encabezados.append(var)
-    for i in range(len(hypotheses)):
-        encabezados.append(hypotheses[i])
-        hypotheses[i] = hypotheses[i].replace("v","or")
-        hypotheses[i] = hypotheses[i].replace("^","and")
-        hypotheses[i] = hypotheses[i].replace("~","not ")
-        hypotheses[i] = hypotheses[i].replace("->","<=")
-    encabezados.append(conclusion)
-
-    n = len(variables)
-    nfilas = 2 ** n
-
-    for i in range (nfilas):
-        fila = []
-        bin = format(i,'016b')
-        bin = bin[-n:]
-        
-        valores = dict()
-        for j in range(n):
-            valores[variables[j]] = int(bin[j])
-            fila.append(bin[j])
-            
-        for j in range(len(hypotheses)):
-            h = hypotheses[j]
-            result = eval(h, {}, valores)
-            print(f"Evaluando: {h} con {valores} = {result}")
-            fila.append(result)
-        
-        fila.append(eval(conclusion, {}, valores))
-        
-        tabla.insert(0, fila)
-        
+    #Guardamos "en el navegador" los encabezados y la tabla
     session['encabezados'] = encabezados
     session['tabla'] = tabla
+    session['renglones_criticos'] = renglones_criticos
+    session['renglones_falla'] = renglones_falla
+    session['argumento_valido'] = argumento_valido
+    session['metodo'] = "renglon_critico"
     
-    return jsonify({"status": "ok"}), 200
+    return jsonify({"status": "ok"}), 200 #regresamos al main.js que todo ok
 
-@app.route('/metodo-tautologia', methods=['POST'])
+@app.route('/metodo-tautologia', methods=['POST']) #se manda a llamar desde main.js cuando se usa el boton de tautologia
 def metodo_tautologia():
+    #Recibimos los datos json que mando el html
     data = request.get_json()
-    hypotheses = data.get('hypotheses', [])
+    arreglo_hipotesis = data.get('hypotheses', [])
     conclusion = data.get('conclusion', '')
-    tabla = []
     
-    variables = set()
+    #Generamos la tabla. Le mandamos las hipotesis, la conclusion y que es con tautologia
+    encabezados, tabla, argumento_valido, renglones_criticos, renglones_falla = generar_tabla_verdad(arreglo_hipotesis, conclusion, "tautologia")
     
-    for i in range(len(hypotheses)):
-        variables.update(extraer_variables(hypotheses[i]))
-        
-    variables = list(variables)
-    variables.sort()
-        
-    encabezados = []
-    for var in variables:
-        encabezados.append(var)
-    for i in range(len(hypotheses)):
-        encabezados.append(hypotheses[i])
-        hypotheses[i] = hypotheses[i].replace("v","or")
-        hypotheses[i] = hypotheses[i].replace("^","and")
-        hypotheses[i] = hypotheses[i].replace("~","not ")
-        hypotheses[i] = hypotheses[i].replace("->","<=")
-    encabezados.append(conclusion)
-    
-    implicacion_general = ""
-    for h in hypotheses:
-        if implicacion_general:
-            implicacion_general += " and "
-        implicacion_general += "(" + h + ")"
-    implicacion_general = "(" + implicacion_general + ") <= " + conclusion
-    
-    encabezados.append("Conjunción de Hipotesis -> Conclusion")
-    n = len(variables)
-    nfilas = 2 ** n
-
-    for i in range (nfilas):
-        fila = []
-        bin = format(i,'016b')
-        bin = bin[-n:]
-        
-        valores = dict()
-        for j in range(n):
-            valores[variables[j]] = int(bin[j])
-            fila.append(bin[j])
-            
-        for j in range(len(hypotheses)):
-            h = hypotheses[j]
-            result = eval(h, {}, valores)
-            fila.append(result)
-            
-        fila.append(eval(conclusion, {}, valores))
-        print(f"Evaluando: {implicacion_general} con {valores} = {eval(implicacion_general, {}, valores)}")
-        fila.append(eval(implicacion_general, {}, valores))
-        
-        tabla.insert(0, fila)
-        
+    #Guardamos "en el navegador" los encabezados y la tabla
     session['encabezados'] = encabezados
     session['tabla'] = tabla
+    session['argumento_valido'] = argumento_valido
+    session['renglones_criticos'] = []
+    session['renglones_falla'] = []
+    session['metodo'] = "tautologia"
+    return jsonify({"status": "ok"}), 200 #regresamos al main.js que todo ok
+
+
+
+
+def generar_tabla_verdad(arreglo_hipotesis, conclusion, metodo):
+    variables = set()
+    implicacion_general = ""
     
-    return jsonify({"status": "ok"}), 200
+    #Extraemos las variables de las hipotesis
+    for i in range(len(arreglo_hipotesis)):
+        variables.update(extraer_variables(arreglo_hipotesis[i]))
+    
+    #las ponemos en una lista y las ordenamos
+    variables = list(variables)
+    variables.sort()
+    
+    #Los encabezados son los titulos de las columnas, como "p q r hipotesis1... concluson"
+    encabezados = []
+    
+    #Agregamos las variables a los encabezados, porq son las primeras columnas
+    for var in variables:
+        encabezados.append(var)
+        
+    for i in range(len(arreglo_hipotesis)):
+        encabezados.append(arreglo_hipotesis[i]) #Agregamos las hipotesis a los encabezados, porq son las siguientes columnas
+        #Reemplazamos operadores para que eval los evalue chido
+        arreglo_hipotesis[i] = arreglo_hipotesis[i].replace("v","or")
+        arreglo_hipotesis[i] = arreglo_hipotesis[i].replace("^","and")
+        arreglo_hipotesis[i] = arreglo_hipotesis[i].replace("~","not ")
+        arreglo_hipotesis[i] = arreglo_hipotesis[i].replace("->","<=")   #SON LO MISMO :D checamos las tablas de verdad y implicacion es lo mismo que <= cuando usamos 0's y 1's
+        
+    encabezados.append(conclusion) #Agregamos la conclusion a los encabezados, porq es la ultima columna
+    
+    if metodo == "tautologia": #Si es tautologia, hay que hacer la implicacion gigante para la comprobación
+        #Juntamos todas las hipotesis con ands
+        for h in arreglo_hipotesis:
+            if implicacion_general:
+                implicacion_general += " and "
+            implicacion_general += "(" + h + ")"  #ej "(h1) and (h2) and ... and (hn)"
+        implicacion_general = "(" + implicacion_general + ") <= " + conclusion #ej "((h1) and (h2) and ... and (hn)) <= conclusion"
+        encabezados.append("Conjunción de Hipotesis -> Conclusion") #Agregamos el encabezado de la ultima columna, que es la implicacion gigante
+
+    n = len(variables)
+    nfilas = 2 ** n #Calculamos cuantas filas van a ser
+    tabla = []
+    renglones_criticos = []
+    renglones_falla = []
+    argumento_valido = True
+
+    for i in range (nfilas): #Ciclo for para generar cada fila
+        fila = []
+        binario = format(i,'016b') #Convertimos el numero de fila a binario
+        binario = binario[-n:] #Volteamos el numero bin para que los 0's queden a la derecha
+        
+        #El numero binario representa los valores de las variables en esa fila
+        #Fila 2 -> 110
+        #si tenemos 3 variables, p y q, tomamos p como 1, q como 1 y r como 0
+        
+        resultados_hipotesis = [] 
+        
+        #Creamos un dicc de los valores de las variables en esa fila
+        valores = dict()
+        for j in range(n): 
+            valores[variables[j]] = int(binario[j])  #si variables = ['p', 'q', 'r']  100 -> {'p': 1, 'q': 0, 'r': 0}
+            fila.append(binario[j])
+            #p q r
+            #1 0 0
+        for j in range(len(arreglo_hipotesis)):
+            h = arreglo_hipotesis[j]
+            resultado = eval(h, {}, valores) #Evaluamos la hipotesis usando los valroes de esa fila del dicc
+            resultados_hipotesis.append(resultado)
+            fila.append(resultado)
+            #p q r  p v q   
+            #1 0 0    1
+        resultado_conclusion = eval(conclusion, {}, valores)
+        fila.append(resultado_conclusion) #Evaluamos la conclusion usando los valroes de esa fila del dicc
+        #p q r  p v q   q   
+        #1 0 0    1     0
+        if metodo == "tautologia":
+            check_tautologia = eval(implicacion_general, {}, valores) #Evaluamos la implicacion gigante usando los valroes de esa fila del dicc
+            fila.append(check_tautologia) #Agregamos el resultado de la implicacion gigante a la fila
+            if not check_tautologia:
+                argumento_valido = False #Si la implicacion gigante no es verdadera en alguna fila, el argumento no es valido
+        
+        
+        if metodo == "renglon_critico" and all(resultados_hipotesis): #Si es renglon critico, hay que checar si todas las hipotesis son verdaderas en esa fila
+            renglones_criticos.append(nfilas-i-1) #si es renglon critico, guardamos el numero de renglon, pero como la tabla se va a mostrar al reves, guardamos nfilas-i-1
+            if not resultado_conclusion: 
+                argumento_valido = False #Si es renglon critico y la conclusion no es verdadera en alguna fila, el argumento no es valido
+                renglones_falla.append(nfilas-i-1) #Es un renglon critico que fallo, asi que lo guardamos
+                print(f"Renglon de falla: {nfilas-i-1}")
+        
+        tabla.insert(0, fila) #Insertamos la fila que acabamos de hacer al inicio de la tabla, para que la fila "0" que tiene puros 0's o falsos quede hasta abajo
+        
+    return encabezados, tabla, argumento_valido, renglones_criticos, renglones_falla
 
 def extraer_variables(expresion):
     variables = set()
     for char in expresion:
-        if char.isalpha() and char not in ['v', '^', '~', '>', '-']:
+        if char.isalpha() and char not in ['v', '^', '~', '>', '-']: #Si el caracter es una letra y no es un operador logico, es una variable
             variables.add(char)
     return variables
--- static/js/main.js
+++ static/js/main.js
@@ -1,39 +1,41 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hypothesisInput = document.getElementById('hypothesis');
-    const addHypothesisButton = document.querySelector('button.btn-highlight'); 
+document.addEventListener('DOMContentLoaded', function() { //Esperamos a que la pagina cargue para ejecutar el js
 
-    addHypothesisButton.addEventListener('click', function() {
-        const text = hypothesisInput.value.trim();
-        if (text === '') {
-            alert('Por favor, ingresa una hipótesis.');
+    const hypothesisInput = document.getElementById('hypothesis'); //obtenemos el cuadro de entrada de texto de la hipotesis
+    const addHypothesisButton = document.getElementById('boton-agregar-hypothesis'); //obtenemos el boton de agregar hipotesis
+
+    addHypothesisButton.addEventListener('click', function() { //Esto se ejecuta cuando se hace click en el boton de agregar hipotesis
+        const text = hypothesisInput.value.trim(); //en text guardamos la hipotesis que el usuario recien escribio
+        if (text === '') { //Si no escribio nada
+            alert('Por favor, ingresa una hipótesis.'); 
             return;
         }
 
-        for (let i = 1; i <= 10; i++) {
+        for (let i = 1; i <= 10; i++) { //Recorremos los 10 cuadritos de hipotesis para encontrar el primero vacio
             const hInput = document.getElementById('h' + i);
             if (hInput && hInput.value === '') {
-                hInput.value = text;
+                hInput.value = text; //Ponemos la hipotesis que el usuario escribio en el primer cuadrito vacio
                 hypothesisInput.value = ''; 
                 return;
             }
         }
 
-        alert('Todas las hipótesis están llenas.');
+        alert('Todas las hipótesis están llenas.'); //Si todas estaban llenas
     });
 
 
-    const conclusionInput = document.getElementById('conclusion');
-    const addConclusionButton = document.querySelector('#conclusion + button'); 
-    addConclusionButton.addEventListener('click', function() {
-        const value = conclusionInput.value.trim();
-        if (value === '') {
+    const conclusionInput = document.getElementById('conclusion'); //obtenemos el cuadro de entrada de texto de la conclusion
+    const addConclusionButton = document.getElementById('boton-agregar-conclusion'); //obtenemos el boton de agregar conclusion
+
+    addConclusionButton.addEventListener('click', function() { //Esto se ejecuta cuando se hace click en el boton de agregar conclusion
+        const text = conclusionInput.value.trim(); //en text guardamos la conclusion que el usuario recien escribio
+        if (text === '') { //Si no escribio nada
             alert('Por favor, ingresa una conclusión.');
             return;
         }
 
         const qInput = document.getElementById('Q');
-        if (qInput && qInput.value === '') {
-            qInput.value = value;
+        if (qInput && qInput.value === '') { //Si el cuadrito de conclusion esta vacio, ponemos la conclusion que el usuario escribio
+            qInput.value = text;
             conclusionInput.value = ''; 
         } else {
             alert('La conclusión ya está establecida.');
@@ -41,40 +43,41 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Enviar hipotesis y conclusion
+    //Obtenemos los botones de cada metodo
     const renglonCriticoButton = document.getElementById('renglon-critico');
     const metodoTautologiaButton = document.getElementById('metodo-tautologia');
 
-    renglonCriticoButton.addEventListener('click', function() {
+    renglonCriticoButton.addEventListener('click', function() { //Si hacen click en el boton de renglon critico, qe se haga el metodo de renglon critico
         sendData('/renglon-critico');
     });
 
-    metodoTautologiaButton.addEventListener('click', function() {
+    metodoTautologiaButton.addEventListener('click', function() { //Si hacen click en el boton de metodo tautologia, que se haga el metodo de tautologia
         sendData('/metodo-tautologia');
     });
 
-    function sendData(link) {
+    function sendData(link) { //Funcion que manda todas las hipotesis y cocnlusion al link que e le pase
         const hypotheses = [];
-        for (let i = 1; i <= 10; i++) {
+        for (let i = 1; i <= 10; i++) { //Iteramos por los 10 cuadros de hipotesis y guardamos el contenido de c/u
             const val = document.getElementById('h' + i).value.trim();
             if (val) hypotheses.push(val);
         }
-        const conclusion = document.getElementById('Q').value.trim();
+        const conclusion = document.getElementById('Q').value.trim(); //Guardamos la conclusion
 
-        if (hypotheses.length === 0 || !conclusion) {
+        if (hypotheses.length === 0 || !conclusion) { //Manejo de errores (campos vacios)
             alert('Debes tener al menos una hipótesis y una conclusión.');
             return;
         }
 
-        fetch(link, {
+        fetch(link, { //Mandamos las hipot y concl a la ruta
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json' 
             },
             body: JSON.stringify({ hypotheses, conclusion }) //convirte el objeto a JSON
         })
-        .then(response => {
+        .then(response => { //si llegamos aqui es porque se hizo la tabla de verdad bien y ya está guardada en session
             if (response.ok) {
-                window.location.href = "/resultado";
+                window.location.href = "/resultado"; //vamos a la ruta de mostrar la tabla
             }
         });
     }
